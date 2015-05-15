a710f4df41aa6d4cedb10dffafdf23926ddd01cc
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2015-05-15 09:05:36 -0500

    updats

--- API_Training_Module/authenticate_api1.5.sh
+++ API_Training_Module/authenticate_api1.5.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
-if [ $# -ne 2 ] 
+if [ $# -ne 2 ]
 then
-	echo "USAGE: $0 API_ENDPOINT REFRESH_TOKEN" 
-	echo "Where API_ENDPOINT is your API 1.5 endpoint found in the CM portal."
-	echo "Where REFRESH_TOKEN is your refresh token found in the CM portal."
-	exit 1
+        echo "USAGE: $0 API_ENDPOINT REFRESH_TOKEN" 
+        echo "Where API_ENDPOINT is your API 1.5 endpoint found in the CM portal."
+        echo "Where REFRESH_TOKEN is your refresh token found in the CM portal."
+        exit 1
 fi
 
 my_token_endpoint=${1}
 my_refresh_token=${2}
 
-curl --include \
-  -H "X-API-Version:1.5" \
-  --request POST "$my_token_endpoint" \
-  -d "grant_type=refresh_token" \
-  -d "refresh_token=$my_refresh_token"
+echo "##########"
+echo "Here is the curl command you are sending"
+cat << EOF 
+curl -s --include \
+-H "X-API-Version:1.5" \
+--request POST "$my_token_endpoint" \
+-d "grant_type=refresh_token" \
+-d "refresh_token=$my_refresh_token"
+EOF
+echo "#########"
+echo ""
+
+curl -s --include \
+        -H "X-API-Version:1.5" \
+        --request POST "$my_token_endpoint" \
+        -d "grant_type=refresh_token" \
+        -d "refresh_token=$my_refresh_token"
