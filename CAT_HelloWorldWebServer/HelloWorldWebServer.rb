57c1e212dfc053b3bd50428ea217113f71bc5049
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2016-07-05 13:05:37 -0500

    tweaks

---
+++ CAT_HelloWorldWebServer/HelloWorldWebServer.rb
@@ -0,0 +1,420 @@
+#
+#The MIT License (MIT)
+#
+#Copyright (c) 2014 By Mitch Gerdisch
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+
+#RightScale Cloud Application Template (CAT)
+
+# DESCRIPTION
+# Super basic CAT file to introduce Self-Service and CATs.
+# Uses a "Hello World Web Server" server template which is simply a Base Linux ServerTemplate with
+# a script that installs httpd and drops in an index.html file with a line of text defined by an input.
+#
+# PREREQUISITES and CAT PREPARATION:
+#   Server Template: A server template called "Hello World Web Server" must exist for the account being used.
+#     This server template must have a rightscript called helloworld_rightscript that can be invoked.
+#     The server template must be able to be deployed to the clouds specified in the map_cloud mapping below.
+#   SSH Key:
+#     The account must have an SSH key named "default"
+#       You do not need to know the private key for "default" since you can use your personal SSH key for any access needed.
+
+name 'CHANGEME - Hello World Web Server'
+rs_ca_ver 20131202
+short_description 'Automates the deployment of a simple single VM server.'
+
+##############
+# PARAMETERS 
+# Inputs provided by users when launching the cloud application.
+##############
+
+# Which cloud?
+# Maps to specific cloud below.
+parameter "param_location" do 
+  category "Deployment Options"
+  label "Cloud" 
+  type "string" 
+  description "Cloud to deploy in." 
+  allowed_values "AWS-US-East", "AWS-US-West"
+  default "AWS-US-East"
+end
+
+# What type of instance?
+# Maps to a specific instance type below.
+parameter "param_performance" do 
+  category "Deployment Options"
+  label "Performance profile" 
+  type "string" 
+  description "Compute and RAM" 
+  allowed_values "low", "medium", "high"
+  default "low"
+end
+
+# What text does the user want the web server to display when browser is pointed at it.
+parameter "param_webtext" do 
+  category "Application Options"
+  label "Web Text" 
+  type "string" 
+  description "Text to display on the web server." 
+  default "Hello World!"
+end
+
+# Project ID for the application
+parameter "param_projectid" do
+  category "Project Options"
+  label "Project ID"
+  type "string"
+  description "Project Id for the application."
+  min_length 8
+  max_length 24
+  allowed_pattern "^[0-9a-zA-Z]+$"
+  constraint_description "Must be alphanumeric string of 8 to 24 characters."
+  default "Project1234"  
+end
+
+
+
+##############
+# MAPPINGS   #
+##############
+
+# Maps the user's selected performance level into a specific instance type.
+mapping "map_instance_type" do {
+  "AWS" => {
+    "low" => "m1.small",  
+    "medium" => "m1.medium", 
+    "high" => "c3.large", 
+  },
+}
+end
+
+# Maps the user's selected cloud into a specific cloud or region.
+mapping "map_cloud" do {
+  "AWS-US-East" => {
+    "provider" => "AWS",
+    "cloud" => "us-east-1",
+  },
+  "AWS-US-West" => {
+    "provider" => "AWS",
+    "cloud" => "us-west-1",
+  },
+}
+end
+
+# Account specific mappings
+mapping "map_account" do {
+  "training_account" => {
+    "ssh_key" => "default", # Be sure there is an ssh key called default or change this to an ssh key that does exist for the account. You do NOT need to have access to the private key.
+    "hello_world_script" => "helloworld_rightscript",  # This is the hello world script in the Hello World server template.
+  },
+}
+end
+
+
+##############
+# CONDITIONS #
+##############
+
+# Checks if being deployed in AWS.
+# This is used to decide whether or not to pass an SSH key and security group when creating the servers.
+condition "inAWS" do
+  equals?(map($map_cloud, $param_location,"provider"), "AWS")
+end
+
+
+##############
+# OUTPUTS    #
+##############
+
+output "server_url" do
+  label "Server URL" 
+  category "Connect"
+  default_value join(["http://", @web_server.public_ip_address])
+  description "Access the web server page."
+end
+
+
+##############
+# RESOURCES  #
+##############
+
+resource "sec_group", type: "security_group" do
+  name join(["HelloWorldSecGrp-",@@deployment.href])
+  description "Hello World web server security group."
+  cloud map( $map_cloud, $param_location, "cloud" )
+end
+
+resource "sec_group_rule_http", type: "security_group_rule" do
+  name "HelloWorld Security Group HTTP Rule"
+  description "Allow HTTP access."
+  source_type "cidr_ips"
+  security_group @sec_group
+  protocol "tcp"
+  direction "ingress"
+  cidr_ips "0.0.0.0/0"
+  protocol_details do {
+    "start_port" => "80",
+    "end_port" => "80"
+  } end
+end
+
+resource "sec_group_rule_ssh", type: "security_group_rule" do
+  name "HelloWorld Security Group SSH Rule"
+  description "Allow SSH access."
+  source_type "cidr_ips"
+  security_group @sec_group
+  protocol "tcp"
+  direction "ingress"
+  cidr_ips "0.0.0.0/0"
+  protocol_details do {
+    "start_port" => "22",
+    "end_port" => "22"
+  } end
+end
+
+
+resource "web_server", type: "server" do
+  name "Hello World Web Server"
+  cloud map( $map_cloud, $param_location, "cloud" )
+  instance_type  map( $map_instance_type, map( $map_cloud, $param_location,"provider"), $param_performance)
+  server_template find("Hello World Web Server")
+  ssh_key switch($inAWS, map($map_account, "training_account", "ssh_key"), null)
+  security_groups @sec_group
+  inputs do {
+    "WEBTEXT" => join(["text:", $param_webtext])
+  } end
+end
+
+
+###############
+## Operations #
+###############
+
+# Allows user to modify the web page text.
+operation "Update Web Page" do
+  description "Modify the web page text."
+  definition "update_webtext"
+end
+
+operation "enable" do
+  description "Post launch actions"
+  definition "post_launch"
+end
+
+##############
+# Definitions#
+##############
+
+# 
+# Perform custom enable operation.
+# 
+define post_launch($param_projectid) do
+  #Add project id tag to the server
+  $tags=[join(["project:id=",$param_projectid])]
+  rs.tags.multi_add(resource_hrefs: @@deployment.servers().current_instance().href[], tags: $tags)
+end
+
+
+#
+# Modify the web page text
+#
+define update_webtext(@web_server, $map_account, $param_webtext) do
+  task_label("Update Web Page")
+  call run_executable(@web_server, {inputs: {WEBTEXT: "text:"+$param_webtext}, rightscript: {name: "helloworld_rightscript"}}) retrieve @task
+end
+
+########## HELPER FUNCTIONS ############
+## Helper definition, runs a script on given server, waits until script completes or fails
+## Raises an error in case of failure
+#define run_script(@target, $right_script_href, $script_inputs) do
+#  @task = @target.current_instance().run_executable(right_script_href: $right_script_href, inputs: $script_inputs)
+#  sleep_until(@task.summary =~ "^(completed|failed)")
+#  if @task.summary =~ "failed"
+#    raise "Failed to run " + $right_script_href
+#  end
+#end
+
+# Run a rightscript or recipe on a server or instance collection.
+#
+# @param @target [ServerResourceCollection|InstanceResourceCollection] the
+#   resource collection to run the executable on.
+# @param $options [Hash] a hash of options where the possible keys are;
+#   * ignore_lock [Bool] whether to run the executable even when the instance
+#     is locked.  Default: false
+#   * wait_for_completion [Bool] Whether this definition should block waiting for
+#     the executable to finish running or fail.  Default: true
+#   * inputs [Hash] the inputs to pass to the run_executable request.  Default: {}
+#   * rightscript [Hash] a hash of rightscript details where the possible keys are;
+#     * name [String] the name of the rightscript to execute
+#     * revision [Int] the revision number of the rightscript to run.
+#       If not supplied the "latest" (which could be HEAD) will be used.
+#     * href [String] if specified href takes prescedence and defines the *exact*
+#       rightscript and revision to execute
+#     * revmatch [String] a ServerTemplate runlist name (one of "boot",
+#       "operational","decomission").  When supplied only the "name" option
+#       is considered and is required.  The RightScript which is executed will
+#       be the one with the same name that is in the specified runlist.
+#   * recipe [String] the recipe name to execute (must be associated with the
+#     @target's ServerTemplate)
+#
+# @return @task [TaskResourceCollection] the task returned by the run_executable
+#   request
+#
+# @see http://reference.rightscale.com/api1.5/resources/ResourceInstances.html#multi_run_executable
+# @see http://reference.rightscale.com/api1.5/resources/ResourceTasks.html
+define run_executable(@target,$options) return @tasks do
+  @tasks = rs.tasks.empty()
+  $default_options = {
+    ignore_lock: false,
+    wait_for_completion: true,
+    inputs: {}
+  }
+
+  $merged_options = $options + $default_options
+
+  # TODO: type() always returns just "collection" reported as line 11 in the doc
+  # https://docs.google.com/a/rightscale.com/spreadsheets/d/1zEqFvhLDygFdxm588LGrHshpBgp41xvIeqjEVigdHto/edit#gid=0
+  @instances = rs.instances.empty()
+  $target_type = to_s(@target)
+  #$target_type = type(@target)
+  #if $target_type == "rs.servers"
+  if $target_type =~ "servers"
+    @instances = @target.current_instance()
+  #elsif $target_type == "rs.instances"
+  elsif $target_type =~ "instances"
+    @instances = @target
+  else
+    raise "run_executable() can not operate on a collection of type "+$target_type
+  end
+
+  $run_executable_params_hash = {inputs: $merged_options["inputs"]}
+  if contains?(keys($merged_options),["rightscript"])
+    if contains?(keys($merged_options["rightscript"]),["revmatch"])
+      if !contains?(keys($merged_options["rightscript"]),["name"])
+        raise "run_executable() requires both 'name' and 'revmatch' when specifying 'revmatch'"
+      end
+      call instance_get_server_template(@instances) retrieve @server_template
+      call server_template_get_rightscript_from_runnable_bindings(@server_template, $merged_options["rightscript"]["name"], {runlist: $merged_options["rightscript"]["revmatch"]}) retrieve $script_href
+      if !$script_href
+        raise "run_executable() unable to find RightScript named "+$merged_options["rightscript"]["name"]+" in the "+$merged_options["rightscript"]["revmatch"]+" runlist of the ServerTempate "+@server_template.name
+      end
+      $run_executable_params_hash["right_script_href"] = $script_href
+    elsif any?(keys($merged_options["rightscript"]),"/(name|href)/")
+      if contains?(keys($merged_options["rightscript"]),["href"])
+        $run_executable_params_hash["right_script_href"] = $merged_options["rightscript"]["href"]
+      else
+        @scripts = rs.right_scripts.get(filter: ["name=="+$merged_options["rightscript"]["name"]])
+        if empty?(@scripts)
+          raise "run_executable() unable to find RightScript with the name "+$merged_options["rightscript"]["name"]
+        end
+        $revision = 0
+        if contains?(keys($merged_options["rightscript"]),["revision"])
+          $revision = $merged_options["rightscript"]["revision"]
+        end
+        $revisions, @script_to_run = concurrent map @script in @scripts return $available_revision,@script_with_revision do
+          $available_revision = @script.revision
+          if $available_revision == $revision
+            @script_with_revision = @script
+          else
+            # TODO: This won't be necessary when RCL assigns the proper empty return
+            # collection type.
+            @script_with_revision = rs.right_scripts.empty()
+          end
+        end
+        if empty?(@script_to_run)
+          raise "run_executable() found the script named "+$merged_options["rightscript"]["name"]+" but revision "+$revision+" was not found.  Available revisions are "+to_s($revisions)
+        end
+        $run_executable_params_hash["right_script_href"] = @script_to_run.href
+      end
+    else
+      raise "run_executable() requires either 'name' or 'href' when executing a RightScript.  Found neither."
+    end
+  elsif contains?(keys($merged_options),["recipe"])
+    $run_executable_params_hash["recipe_name"] = $merged_options["recipe"]
+  else
+    raise "run_executable() requires either 'rightscript' or 'recipe' in the $options.  Found neither."
+  end
+
+  @tasks = @instances.run_executable($run_executable_params_hash)
+
+  if $merged_options["wait_for_completion"]
+    sleep_until(@tasks.summary =~ "^(completed|failed)")
+    if @tasks.summary =~ "failed"
+      raise "Failed to run " + to_s($run_executable_params_hash)
+    end
+  end
+end
+
+# Does some validation and gets the server template for an instance
+#
+# @param @instance [InstanceResourceCollection] the instance for which to get
+#   the server template
+#
+# @return [ServerTemplateReourceCollection] The server template for the @instance
+#
+# @raise a string error message if the @instance parameter is not an instance
+#   collection
+# @raise a string error message if the @instance does not have a server_template
+#   rel
+define instance_get_server_template(@instance) return @server_template do
+  $type = to_s(@instance)
+  if !($type =~ "instance")
+    raise "instance_get_server_template requires @instance to be of type rs.instances.  Got "+$type+" instead"
+  end
+  $stref = select(@instance.links, {"rel": "server_template"})
+  if size($stref) == 0
+    raise "instance_get_server_template can't get the ServerTemplate of an instance which does not have a server_template rel."
+  end
+  @server_template = @instance.server_template()
+end
+
+# Return a rightscript href (or null) if it was found in the runnable bindings
+# of the supplied ServerTemlate
+#
+# @param @server_template [ServerTemplateResourceCollection] a collection
+#   containing exactly one ServerTemplate to search for the specified RightScript
+# @param $name [String] the string name of the RightScript to return
+# @param $options [Hash] a hash of options where the possible keys are;
+#   * runlist [String] one of (boot|operational|decommission).  When supplied
+#     the search will be restricted to the supplied runlist, otherwise all
+#     runnable bindings will be evaulated, and the first result will be returned
+#
+# @return $href [String] the href of the first RightScript found (or null)
+#
+# @raise a string error message if the @server_template parameter contains more
+#   than one (1) ServerTemplate
+define server_template_get_rightscript_from_runnable_bindings(@server_template, $name, $options) return $href do
+  if size(@server_template) != 1
+    raise "server_template_get_rightscript_from_runnable_bindings() expects exactly one ServerTemplate in the @server_template parameter.  Got "+size(@server_template)
+  end
+  $href = null
+  $select_hash = {"right_script": {"name": $name}}
+  if contains?(keys($options),["runlist"])
+    $select_hash["sequence"] = $options["runlist"]
+  end
+  @right_scripts = select(@server_template.runnable_bindings(), $select_hash)
+  if size(@right_scripts) > 0
+    $href = @right_scripts.right_script().href
+  end
+end
+
+
+
