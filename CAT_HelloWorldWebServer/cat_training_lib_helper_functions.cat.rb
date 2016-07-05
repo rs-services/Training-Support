e46ae1f4ee20acaca7b93393d17fe940f0bc97d3
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2016-07-05 14:48:48 -0500

    tweaks

--- CAT_HelloWorldWebServer/cat_training_lib_helper_functions.cat.rb
+++ CAT_HelloWorldWebServer/cat_training_lib_helper_functions.cat.rb
@@ -11,11 +11,9 @@ define run_script(@server, $script_name, $inputs_hash) do
   @script = rs_cm.right_scripts.get(filter: join(["name==",$script_name]))
   $right_script_href=@script.href
 
-  @running_servers = select(@web_tier.current_instances(), {"state":"operational"})
-  @tasks = @server.current_instance().run_executable(right_script_href: $right_script_href, inputs: $inputs_hash)
-  sleep_until(all?(@tasks.summary[], "/^(completed|failed)/"))
-
-  if any?(@tasks.summary[], "/^failed/")
-    raise "Failed to run " + $right_script_href + " on one or more instances"
+  @task = @server.current_instance().run_executable(right_script_href: $right_script_href, inputs: $inputs_hash)
+  
+  if equals?(@tasks.summary, "/^failed/")
+    raise "Failed to run " + $right_script_href + "."
   end
 end
